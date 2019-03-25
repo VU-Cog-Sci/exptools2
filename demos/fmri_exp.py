9ef81938acaba5afc50b2e242891cd6250576653
Lukas Snoek <lukassnoek@gmail.com>
2019-03-25 11:13:58 +0100

    Add wait_n_triggers example

--- demos/fmri_exp.py
+++ demos/fmri_exp.py
@@ -10,7 +10,7 @@ class TestFMRISession(TestSession):
         self.display_text('Waiting for scanner', keys=self.settings['mri'].get('sync', 't'))
         # ^ only real difference with simple_exp
 
-        self.start_experiment()
+        self.start_experiment(wait_n_triggers=4)
 
         for trial in self.trials:
             trial.run()
@@ -21,5 +21,5 @@ class TestFMRISession(TestSession):
 if __name__ == '__main__':
 
     session = TestFMRISession('sub-01', n_trials=10)
-    session.create_trials(durations=(15, 15), timing='frames')
+    session.create_trials(durations=(0.5, .5), timing='seconds')
     session.run()
